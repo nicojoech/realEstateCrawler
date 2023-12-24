2e41dc7e6a2f66261f032bf6022100135a0f41aa
Alexander Lang <alexander.lang1610@gmail.com>
2023-12-24 10:07:42 +0100

    added profile parameters

--- src/profile.js
+++ src/profile.js
@@ -3,5 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function handleUserPage() {
+  document.getElementById("profileFirstName")?.setAttribute("value", localStorage.getItem("firstname") || "First Name");
+  document.getElementById("profileLastName")?.setAttribute("value", localStorage.getItem("lastname") || "Last Name");
   document.getElementById("profileUsername")?.setAttribute("value", localStorage.getItem("username") || "Username");
+  document.getElementById("profileEmail")?.setAttribute("value", localStorage.getItem("email") || "E-Mail");
 }
